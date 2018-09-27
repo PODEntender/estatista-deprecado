9933ef79fbfe3dcf1dc2b577bab84ea9dc483460
Nawarian <nickolas@phpsp.org.br>
2018-09-28 00:42:38 +0200

    Adding asset download in the script

--- src/static-generator.js
+++ src/static-generator.js
@@ -1,18 +1,21 @@
 'use strict'
 
 const fs = require('fs')
-const { dirname, join, parse, resolve: resolvePath } = require('path')
+const { dirname, join, parse } = require('path')
 const { execSync } = require('child_process')
 
 const sleep = require('./utils/sleep')
 const http = require('./utils/http')
 const transformer = require('./transformer')
+const cheerio = require('cheerio')
 
 const {
   NORMALIZED_URL,
   INTERVAL_TO_NEXT_PAGE,
   INTERVAL_TO_RETRY,
 } = process.env
+
+const isHtml = url => parse(url).ext === '.html'
 
 const generateDestinationFile = url => {
   const { dir, base, ext } = parse(url);
@@ -23,42 +26,76 @@ const generateDestinationFile = url => {
 
 const generateDestinationDir = file => dirname(file)
 
-module.exports = async function fetchAndSaveOnlinePages(files) {
-  // const files = _files.filter(file => file.match(/news-podcast/))
-  const file = files.pop()
-  console.log(`Fetching page ${file}...`)
+const fetchUrl = url => http.get(url, {
+  transformResponse: [],
+  responseType: 'stream',
+})
+
+const sanitizeContent = content => transformer.transformInternalLinks(content)
+
+const extractAssetUrls = html => {
+  const urls = []
+  const $ = cheerio.load(html)
+
+  $('img[src],script[src*="//podentender.com"]').toArray().forEach(elm => urls.push(elm.attribs.src))
+
+  $('link[href*="//podentender.com"][href*=".css"]').toArray().forEach(elm => urls.push(elm.attribs.href))
+
+  return urls
+}
+
+module.exports = async function fetchAndSaveOnlinePages(urls) {
+  const url = urls.pop()
+  console.log(`Fetching page ${url}...`)
 
   try {
-      const res = await http.get(file, {
-        transformResponse: transformer.transformInternalLinks,
-      })
+    const file = generateDestinationFile(url)
+    const dir = generateDestinationDir(file)
 
-      const path = generateDestinationFile(file)
-      const dir = generateDestinationDir(path)
+    console.log(`Writing page ${url}...`)
 
-      console.log(`Writing page ${file}...`)
-
-      try {
-        if (!fs.existsSync(dir)) {
-          execSync(`mkdir -p ${dir}`)
-        }
-
-        fs.writeFileSync(path, res.data)
-      } catch (e) {
-        console.log(`Failed to save file ${path}. Skipping...`)
+    try {
+      if (false === fs.existsSync(dir)) {
+        execSync(`mkdir -p ${dir}`)
       }
 
-      if (files.length) {
-        console.log(`Fetching next file in ${INTERVAL_TO_NEXT_PAGE} ms...`)
+      const stream = fs.createWriteStream(file)
+      await fetchUrl(url)
+        .then(res => res.data.pipe(stream))
+        .then(
+          stream => new Promise(
+            resolve => stream.on(
+              'close',
+              () => {
+                resolve(fs.readFileSync(file))
+              }
+            )
+          )
+        )
+        .then(content => {
+          if (true === isHtml(file)) {
+            extractAssetUrls(content.toString()).forEach(url => urls.push(url))
 
-        await sleep(INTERVAL_TO_NEXT_PAGE);
-        return fetchAndSaveOnlinePages(files)
-      }
+            return fs.writeFileSync(file, sanitizeContent(content))
+          }
+
+          return fs.writeFileSync(file, content)
+        })
+    } catch (e) {
+      console.log(`Failed to save file ${file}. Skipping...`)
+    }
+
+    if (0 < urls.length) {
+      console.log(`Fetching next file in ${INTERVAL_TO_NEXT_PAGE} ms...`)
+
+      await sleep(INTERVAL_TO_NEXT_PAGE);
+      return fetchAndSaveOnlinePages(urls)
+    }
   } catch (e) {
-    console.log(`Retrying page ${file} in ${INTERVAL_TO_RETRY} ms...`)
+    console.log(`Retrying page ${url} in ${INTERVAL_TO_RETRY} ms...`)
 
-    files.push(file)
+    urls.push(url)
     await sleep(INTERVAL_TO_RETRY)
-    return fetchAndSaveOnlinePages(files)
+    return fetchAndSaveOnlinePages(urls)
   }
 }
--- wp-content/themes/grandportfolio/js/custom-tab.js
+++
@@ -1,1 +0,0 @@
-jQuery(".tabs").tabs();
