7b01e98ed680dabb42040a7d997095ca05cbdbf6
Níckolas Daniel da Silva <nickolas@phpsp.org.br>
2018-10-17 17:42:05 +0200

    Update transformer.js

--- src/transformer.js
+++ src/transformer.js
@@ -2,23 +2,23 @@
 const cheerio = require('cheerio')
 
 const replaceBaseUrl = url => url.replace(
-  /\/\/podentender\.com/g,
+  /\/\/internal.podentender\.com/g,
   '//estatista.podentender.com'
 )
 
 const transformInternalLinks = document => {
   const $ = document
 
-  $('link[href*="//podentender.com"][href*=".css"],link[rel*="icon"]')
+  $('link[href*="//internal.podentender.com"][href*=".css"],link[rel*="icon"]')
     .each((i, link) => $(link).attr('href', replaceBaseUrl($(link).attr('href'))))
 
-  $('img[src],script[src*="//podentender.com"]')
+  $('img[src],script[src*="//internal.podentender.com"]')
     .each((i, elm) => $(elm).attr('src', replaceBaseUrl($(elm).attr('src'))))
 
-  $('a[href*="//podentender.com"]')
+  $('a[href*="//internal.podentender.com"]')
     .each((i, a) => $(a).attr('href', replaceBaseUrl($(a).attr('href').replace(/\/$/, ''))))
 
-  $('meta[property*="og:image"][content*="//podentender.com"],meta[name="twitter:image"],meta[name*="TileImage"]')
+  $('meta[property*="og:image"][content*="//internal.podentender.com"],meta[name="twitter:image"],meta[name*="TileImage"]')
     .each((i, meta) => $(meta).attr('content', replaceBaseUrl($(meta).attr('content'))))
 
   $('[style*=url]')
