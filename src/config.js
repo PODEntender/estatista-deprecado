b8d02cef528fabf79f6908c0c54e548f40195522
Nawarian <nickolas@phpsp.org.br>
2018-10-31 22:47:14 +0100

    Update config to transform to estatista.podentender.com

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 module.exports = {
   replaceRules: {
     // Replace all internal links
-    ':\/\/internal.podentender.com': '://podentender.com',
+    ':\/\/internal.podentender.com': '://estatista.podentender.com',
 
     // Undo replace for ajax search
     'name="pp_homepage_url" value="https:\/\/podentender.com': 'name="pp_homepage_url" value="https://internal.podentender.com',
